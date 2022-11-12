206f56fb2a304507ba3d469c237f9a1bc5c8971e
Maksim <maks.mashtaler99@gmail.com>
2022-11-12 16:38:23 +0300

    check

--- todolist/c07_todolist/settings.py
+++ todolist/c07_todolist/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 )
 
 # Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
