e39c4b8f3f5ac34b3cedbc4ef3e9f79c24391f3b
Maksim <maks.mashtaler99@gmail.com>
2022-11-29 15:30:08 +0300

    changed name in bot model

--- todolist/bot/admin.py
+++ todolist/bot/admin.py
@@ -4,8 +4,8 @@ from bot.models import TgUser
 
 
 class BotAdmin(admin.ModelAdmin):
-    list_display = ("telegram_user_id", "telegram_chat_id", "user")
-    search_fields = ("telegram_user_id", "telegram_chat_id", "user")
+    list_display = ("tg_user_id", "tg_chat_id", "user")
+    search_fields = ("tg_user_id", "tg_chat_id", "user")
 
 
 admin.site.register(TgUser, BotAdmin)
